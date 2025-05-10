753eaac8f9e4cf0dec8a2a7a421bda61029cdf7a
shmlv9 <denis2009shumilov@gmail.com>
2025-05-10 14:45:26 +0300

    [Backend] Эндпоинты для работы с друзьями готовы

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.authentication_and_profile.register import register_router
 from api.authentication_and_profile.login import login_router
 from api.authentication_and_profile.profile import profile_router
+from api.friends.friends import friends_router
 
 app = FastAPI()
 
@@ -17,3 +18,4 @@ app.add_middleware(
 app.include_router(register_router)
 app.include_router(login_router)
 app.include_router(profile_router)
+app.include_router(friends_router)
