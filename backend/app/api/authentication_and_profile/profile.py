38476cce0db1df63aa322a8a1ce13301ba86906c
shmlv9 <denis2009shumilov@gmail.com>
2025-05-11 18:16:06 +0300

    Дополнен эндпоинт получения информации о пользователе, теперь возвращает статус дружбы и количество друзей

--- backend/app/api/authentication_and_profile/profile.py
+++ backend/app/api/authentication_and_profile/profile.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
-from api.utils.supabase_client import supabase_client
-from api.utils.functions import get_current_user_id
 from api.authentication_and_profile.models import ProfileUpdateRequest
+from api.utils.functions import get_current_user_id
+from api.utils.supabase_client import supabase_client
+from fastapi import APIRouter, Depends, HTTPException
 
 profile_router = APIRouter(
     tags=["profile"],
@@ -49,12 +49,35 @@ def get_profile(user_id: int, current_user_id: int = Depends(get_current_user_id
     if not result:
         raise HTTPException(status_code=404, detail="User not found")
 
-    if user_id == current_user_id:
-        user_info = result[0]
-        user_info.pop("hashed_password", None)
-        user_info.pop("created_at", None)
-        user_info.pop("avatar_url", None)
+    user_info = result[0]
+
+    friends_response = supabase_client.table("friends").select("id").or_(
+        f"sender_id.eq.{user_id},recipient_id.eq.{user_id}"
+    ).eq("status", True).execute().data
+    user_info["friends_count"] = len(friends_response)
+
+    if user_id != current_user_id:
+        friendship = supabase_client.table("friends") \
+            .select("recipient_id, sender_id, status").or_(
+            f"and(sender_id.eq.{user_id},recipient_id.eq.{current_user_id}),and(sender_id.eq.{current_user_id},"
+            f"recipient_id.eq.{user_id})"
+        ).execute().data
+
+        if not friendship:
+            user_info["friendship_status"] = "not_in_friends"
+        else:
+            record = friendship[0]
+            if record["status"] is True:
+                user_info["friendship_status"] = "in_friends"
+            elif record["recipient_id"] == current_user_id:
+                user_info["friendship_status"] = "application_received"
+            else:
+                user_info["friendship_status"] = "application_sent"
 
         return user_info
-    else:
-        return result[0]
+
+    user_info.pop("hashed_password", None)
+    user_info.pop("created_at", None)
+    user_info.pop("avatar_url", None)
+
+    return user_info
